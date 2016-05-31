01df17a467532e87f557adcb39701a0d56704c02
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-05-31 15:35:04 +0200

    404 page typo done

---
+++ dev/wp-content/themes/integration/404.php
@@ -0,0 +1,11 @@
+<?php
+
+get_header();
+
+?>
+
+      <section class="error404">
+            <h2 class="error404__title">Aie Aie Aie, une erreur 404&nbsp;!</h2>
+            <p class="error404__description">La page demandée n'est pas ou n'est plus disonible.</p>
+            <p class="error404__return">Vous pouvez retourner à <a href="<?php echo home_url('/');?>" rel="home" class="error404__link">la page d'accueil</a></p>
+      </section>
