762b28f6c9ece14964a76b6c1549a4bc765e7bc5
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-05-31 15:40:01 +0200

    404 page done

--- dev/wp-content/themes/integration/404.php
+++ dev/wp-content/themes/integration/404.php
@@ -7,5 +7,5 @@ get_header();
       <section class="error404">
             <h2 class="error404__title">Aie Aie Aie, une erreur 404&nbsp;!</h2>
             <p class="error404__description">La page demandée n'est pas ou n'est plus disonible.</p>
-            <p class="error404__return">Vous pouvez retourner à <a href="<?php echo home_url('/');?>" rel="home" class="error404__link">la page d'accueil</a></p>
+            <p class="error404__return">Vous pouvez retourner sur <a href="<?php echo home_url('/');?>" rel="home" class="error404__link">la page d'accueil</a></p>
       </section>
