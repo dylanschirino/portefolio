d30fc07b937716c0b87a3759413ac225072ad27c
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-09-22 22:48:52 +0200

    adding hover effect in footer icon

--- dev/wp-content/themes/integration/footer.php
+++ dev/wp-content/themes/integration/footer.php
@@ -1,15 +1,18 @@
 <footer class="footer">
-  <div class="footer__container">
-
+  <ul class="footer__container">
+    <li class="footer__element">
     <a class="footer__link footer__link--github" href="https://github.com/dylanschirino" title="Voir mon Github">GitHub</a>
-
+  </li>
+    <li class="footer__element">
     <a class="footer__link footer__link--facebook" href="https://www.facebook.com/dylan.schirino" title="Voir mon facebook">Facebook</a>
-
+  </li>
+  <li class="footer__element">
     <a class="footer__link footer__link--linkedin" href="https://www.linkedin.com/in/dylan-schirino" title="Me voir sur linkedin">Linkedin</a>
-
+  </li>
+  <li class="footer__element">
     <a class="footer__link footer__link--phone" href="tel:+32493526168" title="Me téléphoner?">Téléphone</a>
-
-  </div>
+  </li>
+  </ul>
   <div class="footer__subfooter">
     <p class="subfooter__text">
       Design par Dylan Schirino
